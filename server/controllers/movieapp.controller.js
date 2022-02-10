6beedd0e4e926fbd78a346aa3722db73ff646ca2
franpautt27 <franpautt@gmail.com>
2022-02-09 23:29:11 -0500

    config setup

--- server/controllers/movieapp.controller.js
+++ server/controllers/movieapp.controller.js
@@ -1,15 +1,15 @@
 const pool = require('../db');
 
-const getAllMovies = async (req, res) => {
+const getAllMovies = async (req, res, next) => {
     try {
         const allMovies = await pool.query("SELECT * FROM movies");
         res.send(allMovies.rows);
     } catch (error) {
-        console.log(error.message);
+        next(error);
     }
 }
 
-const getMovie = async (req, res) => {
+const getMovie = async (req, res, next) => {
     try {
         const {id} = req.params;
         const result = await pool.query("SELECT * FROM movies WHERE id = $1",[id]);
@@ -23,11 +23,11 @@ const getMovie = async (req, res) => {
         console.log(result);
         res.send("retrieving a specific movie");
     } catch (error) {
-        console.log(error.message);
+        next(error);
     }
 }
 
-const createMovie = async (req, res) => {
+const createMovie = async (req, res, next) => {
     const {movieName, movieLanguage, synopsis} = req.body;
     
     try {
@@ -39,30 +39,38 @@ const createMovie = async (req, res) => {
         
         res.json(result.rows[0]);
     } catch (error) {
-        res.json({ error: error.message });
+        next(error);
     }
 }
 
-const deleteMovie = async (req, res) => {
+const deleteMovie = async (req, res, next) => {
     const { id } = req.params;
-    console.log(id);
-    const result = await pool.query("DELETE FROM movies WHERE id = $1", [id]);
-    if(result.rowCount === 0) return res.status(404).json({
-        message: "Movie Not Found"
-    })
-    
-    return res.sendStatus(204);
+    try {
+        
+        const result = await pool.query("DELETE FROM movies WHERE id = $1", [id]);
+        if(result.rowCount === 0) return res.status(404).json({
+            message: "Movie Not Found"
+        })
+        
+        return res.sendStatus(204);
+    } catch (error) {
+        next(error);
+    }
 }
 
-const updateMovie = async (req, res) => {
-    const { id } = req.params;
-    const { movieName, movieLanguage, synopsis } = req.body;
-    const result = await pool.query("UPDATE movies SET movieName = $1, movieLanguage = $2, synopsis = $3 WHERE id = $4 RETURNING *",[movieName,movieLanguage, synopsis, id])
-    if(result.rows.length === 0) return res.status(404).json({
-        message: "Movie Not Found"
-    })
-    console.log(result);
-    return res.json(result.rows[0]);
+const updateMovie = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const { movieName, movieLanguage, synopsis } = req.body;
+        const result = await pool.query("UPDATE movies SET movieName = $1, movieLanguage = $2, synopsis = $3 WHERE id = $4 RETURNING *",[movieName,movieLanguage, synopsis, id])
+        if(result.rows.length === 0) return res.status(404).json({
+            message: "Movie Not Found"
+        })
+        console.log(result);
+        return res.json(result.rows[0]);
+    } catch (error) {
+        next(error);
+    }
 }
 
 module.exports = {
--- server/index.js
+++ server/index.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const cors = require('cors');
 
 const movieappRoutes = require('./routes/movieapp.routes');
 
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 
 app.use(movieappRoutes);
+
+app.use((err, req, res, next)=>{
+    return res.json({
+        message: err.message
+    })
+} );
 
 app.listen(3001, ()=>{
     console.log('listening on port 3001');
